ab371270f37c95f12598ea80d09448d821f56fe5
bullet18181 <39156225+bullet18181@users.noreply.github.com>
2018-08-04 18:57:20 +0800

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ session_start();
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Big Bang | Log in</title>
+  <title>Your Quote For Today | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
