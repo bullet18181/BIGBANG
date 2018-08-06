8de57c568fb1ef9fbe208db48805915ed0571fa6
Bullet <tuazon548@gmail.com>
2018-08-06 08:51:00 +0800

    [UPDATE]Copyright

--- index.php
+++ index.php
@@ -433,7 +433,7 @@ $_SESSION['callFrom'] = "index.php";
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2016-2017 <a href="#">Big Bang</a>.</strong> All rights
+    <strong>Copyright &copy; 2016-2018 <a href="#">Your Quote</a>.</strong> All rights
     reserved.
   </footer>
 
--- messages.php
+++ messages.php
@@ -186,8 +186,8 @@ if($result->num_rows > 0) {
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2016-2017 <a href="#">Big Bang</a>.</strong> All rights
-    reserved.
+      <strong>Copyright &copy; 2016-2018<a href="#">Your Quote</a>.</strong> All rights
+      reserved.
   </footer>
 
 </div>
